70bf4a3534968f3abb8939bf8cbc07cd5d79c202
Emad El-Deen <emadeldeen839@gmail.com>
2024-08-30 18:50:39 +0300

    Final commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Start type script
+const autoType = new Typed(".typed", {
+  strings: [
+    ,
+    "I'm Simone Olivia",
+    "I'm a Developer",
+    "I'm a Freelancer",
+    "I'm a Designer",
+  ],
+  typeSpeed: 120,
+  backSpeed: 120,
+  loop: true,
+});
+// End Type Script
